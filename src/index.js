aaf95d116f8af8b6b6d6e638a23ab792dcdcc767
alexander sampson <a.sampson206@gmail.com>
2023-10-19 13:51:35 -0700

    larry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener('DOMContentLoaded', () => {
+    let form = document.querySelector('form'); form.addEventListener('submit', (e) => {
+    e.preventDeault()
+    buildToDo(e.target.new_todo.valur)
+    form.reset()})})
+    function handleToDo(todo){
+    let p = document.creatElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    p.textContent = `${todo}`
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector('#todo_container').appendChild(p)}
+    function handleDelete(e){
+    e.target.parentNode.remove()}
+    
 });
